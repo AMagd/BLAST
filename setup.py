d006f47d58e672ad8a1ee28f6de9a08dba81b295
Ahmed Magd <56494952+AMagd@users.noreply.github.com>
2023-06-13 12:35:42 +0900

    Update setup.py install from requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import setuptools
 import pathlib
 
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = (line.strip() for line in file)
+        return [line for line in lines if line and not line.startswith('#')]
 
 setuptools.setup(
     name='dreamerv2',
@@ -14,7 +18,7 @@ setuptools.setup(
     entry_points={'console_scripts': ['dreamerv2=dreamerv2.train:main']},
     install_requires=[
         'gym[atari]', 'atari_py', 'crafter', 'dm_control', 'ruamel.yaml',
-        'tensorflow', 'tensorflow_probability'],
+        'tensorflow', 'tensorflow_probability'], install_requires=parse_requirements('requirements.txt'),
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
