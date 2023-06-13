639b468e63a8ec83b0539a5c13db3cc0d4871ef2
Ahmed Magd <56494952+AMagd@users.noreply.github.com>
2023-06-13 13:08:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setuptools.setup(
     package_data={'dreamerv2': ['configs.yaml']},
     entry_points={'console_scripts': ['dreamerv2=dreamerv2.train:main']},
     install_requires=[
-        'tensorflow==2.6.0', 'tensorflow_probability==0.14.1', 'ruamel.yaml', 'gym[atari]', 'dm_control', 'pudb', 'gym-minigrid==1.0.2', 'sk-video', 
-        'gym-minigrid==1.0.2', 'gym-notices==0.0.8', 'gymnasium==0.27.1', 'gymnasium-notices==0.0.1', 'matplotlib', 'protobuf==3.19.6', 'keras==2.6.0'],
+        'tensorflow==2.6.0', 'tensorflow_probability==0.14.1', 'ruamel.yaml', 'gym[atari]', 'dm_control', 'pudb', 'sk-video', 
+        'gymnasium-notices==0.0.1', 'matplotlib', 'protobuf==3.19.6', 'keras==2.6.0'],
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
